7e9183e04eb9017ea146403f655e5b10e5d9661e
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-06-03 18:35:50 -0600

    Actualizacion de restricciones

--- Main.py
+++ Main.py
@@ -2,15 +2,15 @@
 from algorithm.PSO import PSO
 from algorithm.DE import DE
 # Limites y restricciones
-from core.Limites import Limite
-from core.Restricciones import Restricciones
+from core.limites import Limite
+from core.restricciones import Restricciones
 
 # Estrategias de velocidad
 from utils.correcion_de_velocidad import dba
 from utils.constantes import ITERATIONS
 
 #Generar reporte CSV 
-from utils.Csv import mejor_solucion_csv
+from utils.csv import mejor_solucion_csv
 
 # Funciones objetivas
 from problems.cec2006problems import (
@@ -28,39 +28,36 @@ from problems.cec2022problems import CEC2022_ZakharovF, CEC2022_RosenbrockF, SUP
 
 
 
-problema = CEC2006_G02()
+problema = CEC2020_RC01()
 
 
 def main():
     for i in range(1, ITERATIONS + 1):
         print(f"Ejecucion: {i}")
-        # pso = PSO(
-        #     Limite.wrapping,  # Restriccion de limites
-        #     problema.fitness,  # Problema a evaluar
-        #     problema.SUPERIOR,  # Límites superiores del individuo
-        #     problema.INFERIOR,  # Límites inferiores del individuo
-        #     Restricciones.aEsMejorQueB_deb,  # Restriccion de problema
-        #     dba,  # Una estrategia de actualizacion de velocidad,
-        #     problema.rest_g,  # Son restricciones de desigualdad para el problema,
-        #     problema.rest_h,  # Son restricciones de igualdad para el problema,
-        # )
-        # # Optimizacion
-        # pso.optimizar()
-        # pso.reporte()
-        # mejor_solucion_csv(pso.gbestParticula,pso.gbestAptitud, pso.gbestViolacion,"Prueba_Wrapping.csv")
-        
-        de = DE(
-            Limite.reflex,
-            problema.fitness,
-            problema.SUPERIOR,
-            problema.INFERIOR,
-            Restricciones.aEsMejorQueB_deb,
-            problema.rest_g,
-            problema.rest_h
+        pso = PSO(
+            Limite.attract,  # Restriccion de limites
+            problema.fitness,  # Problema a evaluar
+            problema.SUPERIOR,  # Límites superiores del individuo
+            problema.INFERIOR,  # Límites inferiores del individuo
+            Restricciones.aEsMejorQueB_deb,  # Restriccion de problema
+            dba,  # Una estrategia de actualizacion de velocidad,
+            problema.rest_g,  # Son restricciones de desigualdad para el problema,
+            problema.rest_h,  # Son restricciones de igualdad para el problema,
         )
-        de.run()
-        de.reporte() 
-
+        # Optimizacion
+        pso.optimizar()
+        pso.reporte()
+        # de = DE (
+        #     Limite.reflex,
+        #     problema.fitness,
+        #     problema.SUPERIOR,
+        #     problema.INFERIOR,
+        #     Restricciones.aEsMejorQueB_deb,
+        #     problema.rest_g,
+        #     problema.rest_h,
+        # )
+        # de.run()
+        # de.reporte()
 
 if __name__ == "__main__":
     main()
